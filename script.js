872ecb46726f0e595c447e834e3455a6fd8d708d
PRASHIL3108 <115726633+PRASHIL3108@users.noreply.github.com>
2023-08-22 20:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const submit = getElementByTagName("input")[2]
+	const fname = getElementByTagName("input")[0]
+	const lname = getElementByTagName("input")[1]
+
+	const click = 
+	alert(fname.value + " " + lname.value)
 
 }
